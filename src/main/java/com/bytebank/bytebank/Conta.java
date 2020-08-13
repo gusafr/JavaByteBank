cef385234841f40c0cc4e1cac553d9da3c71c6f5
Gustavo Freitas <gusafr@gmail.com>
2020-08-13 19:09:20 -0300

    Initial commit

---
+++ src/main/java/com/bytebank/bytebank/Conta.java
@@ -0,0 +1,8 @@
+package com.bytebank.bytebank;
+
+class Conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+}
---
+++ src/test/java/com/bytebank/bytebank/BytebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebank.bytebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
