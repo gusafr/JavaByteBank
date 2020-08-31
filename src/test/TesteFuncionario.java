3d664a92f271a439430ba61e3ce8f07428495405
Gustavo Freitas <gusafr@gmail.com>
2020-08-31 17:38:06 -0300

    teste da classe Funcionario

---
+++ src/test/TesteFuncionario.java
@@ -0,0 +1,16 @@
+import com.bytebank.bytebank.Funcionario;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TesteFuncionario {
+
+    @Test
+    public void deveValidarBonificacaoFuncionario() {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setSalario(1000.0);
+        
+        assertEquals(100.0, funcionario.getBonificacao(), 10000.0);
+
+    }
+
+}
